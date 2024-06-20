62bdd38e0598e70c395f9791bb5c4b761e066054
Aitor Bit <62242295+Aitor-bit@users.noreply.github.com>
2024-06-20 21:14:55 +0200

    Update server1.sh

--- mid/server1.sh
+++ mid/server1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip r add 172.30.0.0/16 via 172.31.0.2	# Añadir ruta
+ip r add 172.30.0.0/16 via 172.31.0.2	# Add route
 ip r add 172.32.0.0/16 via 172.31.0.2
 ip r add 172.33.0.0/16 via 172.31.0.2
 /bin/bash
