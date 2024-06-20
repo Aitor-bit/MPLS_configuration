00d3d1892a75c629b155096c770fe0066f046418
Aitor Bit <62242295+Aitor-bit@users.noreply.github.com>
2024-06-20 21:25:48 +0200

    Update router1.sh

--- big/router1.sh
+++ big/router1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ip l add blue type vrf table 2					# Crear VRF blue
-ip l set dev blue up						# Activar VRF
-ip l set dev eth1 master blue					# Asociar eth1 a blue
-ip r add 172.31.0.0/16 encap mpls 302 via 172.20.0.2 vrf blue	# Encaupsular
-ip r add 172.32.0.0/16 encap mpls 402 via 172.20.0.2 vrf blue	# Encaupsular
-ip r add 172.33.0.0/16 encap mpls 502 via 172.20.0.2 vrf blue	# Encaupsular
-ip -f mpls route add 201 dev blue				# Desencapsular
+ip l add blue type vrf table 2					# Create blue VRF
+ip l set dev blue up						# Activate VRF
+ip l set dev eth1 master blue					# Associate eth1 to blue
+ip r add 172.31.0.0/16 encap mpls 302 via 172.20.0.2 vrf blue	# Encapsulate
+ip r add 172.32.0.0/16 encap mpls 402 via 172.20.0.2 vrf blue	# Encapsulate
+ip r add 172.33.0.0/16 encap mpls 502 via 172.20.0.2 vrf blue	# Encapsulate
+ip -f mpls route add 201 dev blue				# Decapsulate
 
 ip l add red type vrf table 3
 ip l set dev red up
