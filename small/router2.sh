eed3cbb2250dfafc4133bf7287779c280bf2f26e
Aitor Bit <62242295+Aitor-bit@users.noreply.github.com>
2024-06-20 21:26:54 +0200

    Update router2.sh

--- small/router2.sh
+++ small/router2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip -f mpls route add 102 as 201 via inet 172.20.0.3	# Cambio de etiqueta
+ip -f mpls route add 102 as 201 via inet 172.20.0.3	# Label switch
 ip -f mpls route add 302 as 203 via inet 172.21.0.3
 
 ip -f mpls route add 112 as 211 via inet 172.20.0.3
