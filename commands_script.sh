c553e285ba3af066fbfca514b56e857e299486db
Aitor Bit <62242295+Aitor-bit@users.noreply.github.com>
2024-06-20 02:01:02 +0200

    Create commands_script.sh

---
+++ commands_script.sh
@@ -0,0 +1,32 @@
+# Use this to get the IP addresses of the Docker containers
+docker inspect [CONTAINER ID] | grep "IPAddress"
+
+# Install kmod
+apt install kmod
+
+# Load kernel modules
+modprobe mpls_router
+modprobe mpls_iptunnel
+modprobe mpls_gso
+modprobe dummy
+
+# Write in the MPLS load file, so that it loads some modules automatically each time the system is started
+cat >/etc/modules-load.d/mpls.conf <<EOF
+mpls_router
+mpls_iptunnel
+mpls_gso
+dummy
+EOF
+
+# Write in the MPLS config file some necessary configurations
+cat >/etc/sysctl.d/90-mpls-router.conf <<EOF
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.rp_filter=0
+net.mpls.platform_labels=1048575
+net.ipv4.tcp_l3mdev_accept=1
+net.ipv4.udp_l3mdev_accept=1
+net.mpls.conf.lo.input=1
+EOF
+
+# Apply these changes
+sysctl -p /etc/sysctl.d/90-mpls-router.conf
